9200eb20652a41c6708f834919736af14c1a5779
sebacampos <seba.campos.012@gmail.com>
2020-08-24 21:03:38 -0400

    add missing img sizes & url missing og tags

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,22 +1,26 @@
 export default {
     title:"DGS Taller | Chapería, Pintura y Mecánica del Automóvil.", // max: 65
     description: '📍Dirección Tte. Villalba 392 c/ Itá. 📞 Teléfonos 021 290924 📲0981568535. Chapa, Pintura y Mecánica del Automóvil. Trabajamos con compañías de seguros', // max:155
-    // canonical: "https://dgs.com.py/",
+    canonical: "https://dgs.vercel.app/",
     openGraph: {
         type: 'website',
         locale: 'es_LA',
-        // url: 'https://dgs.com.py/',
+        url: 'https://dgs.vercel.app/',
         site_name: 'DGS Taller',
         title: 'DGS Taller 🚘🛠', // max 35
         description: 'Taller de Chapería, Pintura y Mecánica del Automóvil.', // max 65 
-        images: [ // less than 300KB and minimum dimensions of 300 x 200
+        images: [
             {
                 url: 'https://dgs.vercel.app/images/logo2.jpg',
                 alt: 'DGS Logo',
+                width: 1269,
+                height: 1269,
             },
             {
                 url: 'https://dgs.vercel.app/images/logo.jpg',
                 alt: 'DGS Logo',
+                width: 3263,
+                height: 1269,
             },
         ],
     },
