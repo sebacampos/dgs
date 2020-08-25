8a87821dabbbf149f12061a76d5ac916a9115889
sebacampos <seba.campos.012@gmail.com>
2020-08-24 20:43:15 -0400

    improve og tags for sharing

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,30 +1,24 @@
 export default {
-    title: "Using More of Config",
-    description: "This example uses more of the available config options.",
+    title:"DGS Taller | Chapería, Pintura y Mecánica del Automóvil.", // max: 65
+    description: '📍Dirección Tte. Villalba 392 c/ Itá. 📞 Teléfonos 021 290924 📲0981568535. Chapa, Pintura y Mecánica del Automóvil. Trabajamos con compañías de seguros', // max:155
     // canonical: "https://dgs.com.py/",
     openGraph: {
         type: 'website',
-        locale: 'es_IE',
+        locale: 'es_LA',
         // url: 'https://dgs.com.py/',
         site_name: 'DGS Taller',
-        title: 'DGS Taller.',
-        description: 'Taller de Chapería, Pintura y Mecánica del Automóvil.',
-        // images: [
-        //     {
-        //         url: 'https://www.example.ie/og-image-01.jpg',
-        //         width: 800,
-        //         height: 600,
-        //         alt: 'DGS Logo',
-        //     },
-        //     {
-        //         url: 'https://www.example.ie/og-image-02.jpg',
-        //         width: 900,
-        //         height: 800,
-        //         alt: 'DGS Logo',
-        //     },
-        //     { url: 'https://www.example.ie/og-image-03.jpg' },
-        //     { url: 'https://www.example.ie/og-image-04.jpg' },
-        // ],
+        title: 'DGS Taller 🚘🛠', // max 35
+        description: 'Taller de Chapería, Pintura y Mecánica del Automóvil.', // max 65 
+        images: [ // less than 300KB and minimum dimensions of 300 x 200
+            {
+                url: 'https://dgs.vercel.app/images/logo2.jpg',
+                alt: 'DGS Logo',
+            },
+            {
+                url: 'https://dgs.vercel.app/images/logo.jpg',
+                alt: 'DGS Logo',
+            },
+        ],
     },
     // facebook: {
     //     appId: '',
